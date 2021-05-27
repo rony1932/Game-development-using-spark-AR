59c56831b7acd631706948c31e2dccae80835d92
186263 <80954151+186263@users.noreply.github.com>
2021-05-27 23:37:27 +0530

    Add files via upload

---
+++ scripts/script(1).js
@@ -0,0 +1,45 @@
+/**
+ * (c) Facebook, Inc. and its affiliates. Confidential and proprietary.
+ */
+
+//==============================================================================
+// Welcome to scripting in Spark AR Studio! Helpful links:
+//
+// Scripting Basics - https://fb.me/spark-scripting-basics
+// Reactive Programming - https://fb.me/spark-reactive-programming
+// Scripting Object Reference - https://fb.me/spark-scripting-reference
+// Changelogs - https://fb.me/spark-changelog
+//
+// For projects created with v87 onwards, JavaScript is always executed in strict mode.
+//==============================================================================
+
+// How to load in modules
+const Scene = require('Scene');
+
+// Use export keyword to make a symbol available in scripting debug console
+export const Diagnostics = require('Diagnostics');
+
+const Patches = require('Patches');
+
+
+Promise.all([
+
+    Scene.root.findFirst('TimerText'),
+
+]).then(function (results) {
+
+    const timerCountText = results[0];
+
+    Patches.outputs.getScalar('Timer').then(timerObj => {
+
+        timerObj.monitor().subscribe(function (timerEvent) {
+
+            timerCountText.text = timerEvent.newValue.toFixed(2).toString();
+
+        });
+
+    });
+
+
+});
+
